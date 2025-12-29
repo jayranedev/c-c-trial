ff88d8dda09945b93b1f19744bcf6175613fe113
Jay Rane <127013993+Jay-shm@users.noreply.github.com>
2025-12-29 21:07:32 +0530

    5

---
+++ 5) Area of circle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    printf("Enter the radius of circle: ");
+    int r;
+    scanf("%d",&r);
+    printf("Area of the circle is: %.2f", 3.14*r*r);
+    return 0;
+}
